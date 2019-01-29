dec6e710e142a1c39a6fd68e19661a6b711d47fd
Ricardo Borges <ricardo93borges@gmail.com>
2019-01-29 17:19:40 -0200

    Added more tests

--- src/reducers/counters/counters.test.js
+++ src/reducers/counters/counters.test.js
@@ -1,16 +1,63 @@
 import counters from './index'
 import { expect } from 'chai'
+import deepFreeze from 'deep-freeze'
 
 it('counters should be a function', () => {
     expect(counters).to.be.a('function')
 })
 
 it('Should add a counter', () => {
-    const before = []
-    const action = { type: 'ADD_COUNTER' }
+    const before = deepFreeze([])
+    const action = deepFreeze({ type: 'ADD_COUNTER' })
     const after = [0]
-
     expect(counters(before, action)).to.be.deep.equal(after)
 })
 
- 
+it('Should add a counter again', () => {
+    const before = deepFreeze([0,1])
+    const action = deepFreeze({ type: 'ADD_COUNTER' })
+    const after = [0,1,0]
+    expect(counters(before, action)).to.be.deep.equal(after)
+})
+
+it('Should remove a counter', () => {
+    const before = deepFreeze([0,1,2])
+    const action = deepFreeze({ type: 'REMOVE_COUNTER', index: 1 })
+    const after = [0,2]
+    expect(counters(before, action)).to.be.deep.equal(after)
+})
+
+it('Should remove a counter again', () => {
+    const before = deepFreeze([1,1])
+    const action = deepFreeze({ type: 'REMOVE_COUNTER', index: 0 })
+    const after = [1]
+    expect(counters(before, action)).to.be.deep.equal(after)
+})
+
+it('Should increment a counter', () => {
+    const before = deepFreeze([0,0])
+    const action = deepFreeze({ type: 'INCREMENT', index: 0 })
+    const after = [1,0]
+    expect(counters(before, action)).to.be.deep.equal(after)
+})
+
+it('Should increment another counter', () => {
+    const before = deepFreeze([1,0])
+    const action = deepFreeze({ type: 'INCREMENT', index: 1 })
+    const after = [1,1]
+    expect(counters(before, action)).to.be.deep.equal(after)
+})
+
+it('Should decrement a counter', () => {
+    const before = deepFreeze([0,2,1])
+    const action = deepFreeze({ type: 'DECREMENT', index: 2 })
+    const after = [0,2,0]
+    expect(counters(before, action)).to.be.deep.equal(after)
+})
+
+it('Should decrement another counter', () => {
+    const before = deepFreeze([0,2,0])
+    const action = deepFreeze({ type: 'DECREMENT', index: 1 })
+    const after = [0,1,0]
+    expect(counters(before, action)).to.be.deep.equal(after)
+})
--- src/reducers/counters/index.js
+++ src/reducers/counters/index.js
@@ -1,3 +1,14 @@
-export default () => {
-    return [0]
+export default (state, action) => {
+    switch (action.type){
+        case 'ADD_COUNTER':
+            return state.concat(0)
+        case 'REMOVE_COUNTER':
+            return state.filter((_, index) => index !== action.index)
+        case 'INCREMENT':
+            return state.map((c, i) => i === action.index ? c + 1 : c)
+        case 'DECREMENT':
+            return state.map((c, i) => i === action.index ? c - 1 : c)
+        default:
+            return []        
+    }
 }
